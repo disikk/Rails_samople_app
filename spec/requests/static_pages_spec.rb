3b7918945e64e863767ffed398718938230fd31f
Denis <thedisik@gmail.com>
2016-02-25 18:40:27 +0700

    18:40 25.02.16

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,11 +5,11 @@ describe "StaticPages" do
 
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title(base_title)
     end
   end
@@ -17,11 +17,11 @@ describe "StaticPages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title(base_title)
     end
   end
@@ -29,11 +29,23 @@ describe "StaticPages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
+      expect(page).to have_title(base_title)
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+    it "should have the right title" do
+      visit contact_path
       expect(page).to have_title(base_title)
     end
   end
